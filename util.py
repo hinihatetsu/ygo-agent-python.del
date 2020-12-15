b9777e2ca4e10f878a868575f437b27ebbdf6d79
hinihatetsu <shuheinittalr@gmail.com>
2020-12-16 01:07:11 +0900

    add notrain option

--- util.py
+++ util.py
@@ -1,5 +1,4 @@
 import argparse
-from argparse import Namespace
 from typing import NamedTuple
 
 
@@ -10,18 +9,20 @@ class LaunchInfo(NamedTuple):
     host: str
     port: int
     version: int
+    notrain: bool
 
 
 def load_args() -> LaunchInfo:
     parser = argparse.ArgumentParser()
-    parser.set_defaults(name='AI', host='127.0.0.1', port=7911, version=VERSION)
+    parser.set_defaults(name='AI', host='127.0.0.1', port=7911, version=VERSION, notrain=False)
     parser.add_argument('--name', type=str, help="AI's name (default: %(default)s)")
     parser.add_argument('--deck', type=str, help='deck name', required=True)
     parser.add_argument('--host', type=str, help='host adress (default: %(default)s)')
     parser.add_argument('--port', type=int, help='port (default: %(default)s)')
     parser.add_argument('--version', type=int, help='version (default: %(default)s)')
-    args: Namespace = parser.parse_args()
-    return LaunchInfo(args.name, args.deck, args.host, args.port, args.version)
+    parser.add_argument('--notrain', action='store_true', help='no train mode (default: %(default)s')
+    args: argparse.Namespace = parser.parse_args()
+    return LaunchInfo(args.name, args.deck, args.host, args.port, args.version, args.notrain)
 
 
 def error(message: str, exit_code: int=1) -> None:
