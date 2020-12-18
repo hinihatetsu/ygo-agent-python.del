275dc6a0a7fead0078857536a5efff0667f7ccfd
hinihatetsu <72799696+hinihatetsu@users.noreply.github.com>
2020-12-18 18:33:25 +0900

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-if __name__ == '__main__':
-    from pyYGO.enums import Phase, Player
-    from pyYGO.duel import Duel
-    from pyYGOAgent.ANN import _create_basic
-    duel = Duel()
-    print(_create_basic(duel))
-    duel._turn_player = Player.OPPONENT
-    duel._phase = Phase.END
-    print(_create_basic(duel))
