b62772667ad968bc421c3b6c46263847e2697a3b
hinihatetsu <shuheinittalr@gmail.com>
2021-04-21 17:33:31 +0000

    tf-agents.agent

--- pyYGOagent/agent.py
+++ pyYGOagent/agent.py
@@ -1,303 +1,227 @@
-import random
-import datetime
-import csv
-import math
-import copy
-from pathlib import Path
+import os
+import tempfile
+from typing import List
 
-from pyYGO import Duel, Card, Zone, Deck, Location
-from pyYGO.phase import MainPhase, BattlePhase
-from pyYGO.enums import CardPosition, Player, Attribute, Race
-from pyYGOenvironment import GameClient, GamePlayer
-from .action import Action, Choice
-from .brain import AgentBrain
-from .flags import UsedFlag
+import numpy as np
+import tensorflow as tf
+import reverb
+
+from tf_agents.agents.ddpg import critic_network
+from tf_agents.agents.sac.sac_agent import SacAgent
+from tf_agents.agents.sac.tanh_normal_projection_network import TanhNormalProjectionNetwork
+from tf_agents.metrics import py_metrics
+from tf_agents.networks import actor_distribution_network
+from tf_agents.policies.py_tf_eager_policy import PyTFEagerPolicy
+from tf_agents.policies import random_py_policy
+from tf_agents.replay_buffers.reverb_replay_buffer import ReverbReplayBuffer
+from tf_agents.replay_buffers.reverb_utils import ReverbAddTrajectoryObserver
+from tf_agents.train import actor, learner, triggers
+from tf_agents.train.utils import train_utils, spec_utils
+
+from pyYGOenv import YGOEnv
+
+tempdir: str = tempfile.gettempdir()
+
+_initial_collect_episodes = 10 # @param {type:"integer"}
+_collect_steps_per_iteration = 1 # @param {type:"integer"}
+_replay_buffer_capacity = 10000 # @param {type:"integer"}
+
+_batch_size = 256 # @param {type:"integer"}
+
+_log_interval = 500 # @param {type:"integer"}
+
+_num_eval_episodes = 5 # @param {type:"integer"}
+_eval_interval = 10000 # @param {type:"integer"}
+
+_policy_save_interval = 5000 # @param {type:"integer"}
 
 
+class DuelAgent:
+    """ Duel agent with SAC algorithm """
+    def __init__(self, collect_env: YGOEnv, eval_env: YGOEnv) -> None:
+        self._collect_env: YGOEnv = collect_env
+        self._eval_env: YGOEnv = eval_env
+        
+        # hyper parameters
+        self._actor_fc_layer_params: List[int] = [512, 256]
+        self._critic_joint_fc_layer_params: List[int] = [512, 256]
+        self._critic_learning_rate: float = 3e-4
+        self._actor_learning_rate: float = 3e-4
+        self._alpha_learning_rate: float = 3e-4
+        self._target_update_tau: float = 0.005
+        self._target_update_period: int = 1
+        self._gamma: float = 0.99
+        self._reward_scale_factor: float = 1.0
+        table_name: str = 'uniform_table'
 
-class DuelAgent(GamePlayer):
-    _MAX_MATCH: int = 1000
-    def __init__(self, deck_name: str, no_train: bool=True) -> None:
-        self._deck: Deck = Deck(deck_name)
-        self._brain: AgentBrain = AgentBrain(self._deck)
-        self._usedflag: UsedFlag = UsedFlag(self._deck)
-        self._no_train: bool = no_train
+        # Agent
+        self._train_step = train_utils.create_train_step()
+        self._agent: SacAgent = self._create_agent()
+        # reverb
+        self._reverb_server: reverb.Server = self._create_reverb_server(table_name)
+        self._reverb_replay: ReverbReplayBuffer = self._create_replay_buffer(table_name)
+        # policy
+        self._eval_policy: PyTFEagerPolicy = PyTFEagerPolicy(self._agent.policy, use_tf_function=True)
+        self._collect_policy: PyTFEagerPolicy = PyTFEagerPolicy(self._agent.collect_policy, use_tf_function=True)
+        # actor
+        self._rb_observer: ReverbAddTrajectoryObserver = self._create_rb_observer(table_name)
+        self._collect_actor: actor.Actor = self._create_collect_actor()
+        self._eval_actor: actor.Actor = self._create_eval_actor()
+        # learner
+        self._agent_learner: learner.Learner = self._create_agent_learner()
 
-        self._duel: Duel = None
-        self._duel_cache: Duel = None
-        self._match_count: int = 0
-        self._wins: int = 0
-        self._match_result: list[bool] = []
 
+    def _create_agent(self) -> SacAgent:
+        observation_spec, action_spec, time_step_spec = spec_utils.get_tensor_specs(self._collect_env)
+
+        critic_net = critic_network.CriticNetwork(
+            (observation_spec, action_spec),
+            observation_fc_layer_params=None,
+            action_fc_layer_params=None,
+            joint_fc_layer_params=self._critic_joint_fc_layer_params,
+        )
+
+        actor_net = actor_distribution_network.ActorDistributionNetwork(
+            observation_spec,
+            action_spec,
+            fc_layer_params=self._actor_fc_layer_params,
+            continuous_projection_net=TanhNormalProjectionNetwork
+        )
+
+        tf_agent = SacAgent(
+            time_step_spec,
+            action_spec,
+            actor_network=actor_net,
+            critic_network=critic_net,
+            actor_optimizer=tf.compat.v1.train.AdamOptimizer(learning_rate=self._actor_learning_rate),
+            critic_optimizer=tf.compat.v1.train.AdamOptimizer(learning_rate=self._critic_learning_rate),
+            alpha_optimizer=tf.compat.v1.train.AdamOptimizer(learning_rate=self._alpha_learning_rate),
+            target_update_tau=self._target_update_tau,
+            target_update_period=self._target_update_period,
+            td_errors_loss_fn=tf.math.squared_difference,
+            gamma=self._gamma,
+            reward_scale_factor=self._reward_scale_factor,
+            train_step_counter=self._train_step
+        )
+        tf_agent.initialize()
+
+        return tf_agent
+
+
+    def _create_reverb_server(self, table_name: str) -> reverb.Server:
+        table = reverb.Table(
+            table_name,
+            max_size=_replay_buffer_capacity,
+            sampler=reverb.selectors.Uniform(),
+            remover=reverb.selectors.Fifo(),
+            rate_limiter=reverb.rate_limiters.MinSize(1)
+        )
+        return reverb.Server([table])
+
+    
+    def _create_replay_buffer(self, table_name: str) -> ReverbReplayBuffer:
+        return ReverbReplayBuffer(
+            self._agent.collect_data_spec,
+            sequence_length=2,
+            table_name=table_name,
+            local_server=self._reverb_server
+        )
+
+
+    def _create_rb_observer(self, table_name: str) -> ReverbAddTrajectoryObserver:
+        return ReverbAddTrajectoryObserver(
+            self._reverb_replay.py_client,
+            table_name,
+            sequence_length=2,
+            stride_length=1
+        )
+
+
+    def _create_collect_actor(self) -> actor.Actor:
+
+        initial_collect_actor = actor.Actor(
+            self._collect_env,
+            random_py_policy.RandomPyPolicy(self._collect_env.time_step_spec(), self._collect_env.action_spec()),
+            self._train_step,
+            episodes_per_run=_initial_collect_episodes,
+            observers=[self._rb_observer]
+        )
+        initial_collect_actor.run()
         
 
-    def set_client(self, client: GameClient) -> None:
-        """ set client where agent play """
-        self._client = client
-        client.set_gameplayer(self)
-        self._duel = client.get_duel()
-
-
-    def run(self):
-        """ agent starts to run """
-        if self._client is None:
-            raise Exception('GameClient not set. Call DuelAgent.set_client().')
-        self._client.set_deck(self._deck)
-        self._client.start()
-        self._dump_match_result()
-        if not self._no_train:
-            self._brain.save_networks()
+        return actor.Actor(
+            self._collect_env,
+            self._collect_policy,
+            self._train_step,
+            episodes_per_run=1,
+            metrics=actor.collect_metrics(10),
+            summary_dir=os.path.join(tempdir, learner.TRAIN_DIR),
+            observers=[self._rb_observer, py_metrics.EnvironmentSteps()]
+        )
 
     
-    def on_start(self) -> None:
-        self._duel_cache = None
-
-    
-    def on_new_turn(self) -> None:
-        self._usedflag.reset()
-        if self._duel.turn_player == Player.ME:
-            reward = self._calculate_reward()
-            self._brain.feedback(reward)
-    
-
-    def on_new_phase(self) -> None:
-        pass
-
-
-    def on_win(self, win: bool) -> None:
-        reward = self._calculate_reward()
-        self._brain.feedback(reward)
-        
-    
-    def on_rematch(self, win_on_match: bool) -> bool:
-        self._match_count += 1
-        self._wins += 1 if win_on_match else 0
-        self._match_result.append(win_on_match)
-        assert len(self._match_result) == self._match_count, "len(self._match_result) != match_count"
-        
-        self._print_match_result(win_on_match)
-        if self._no_train:
-            self._brain.clear_memory()
-        else:
-            self._brain.train()
-        return True if self._match_count < self._MAX_MATCH else False
-
-    
-    def _calculate_reward(self) -> float:
-        if self._duel_cache is None:
-            self._duel_cache = self._duel
-
-        score: list[float] = [0, 0]
-        for p in (Player.ME, Player.OPPONENT):
-            field = (self._duel.field[p].field_count - self._duel_cache.field[p].field_count) 
-            life = -(self._duel.life[p^1] - self._duel_cache.life[p^1]) / 1000
-
-            score[p] = 1 / (1 + math.exp(-(life + field)))
-
-        reward: float = score[Player.ME] - score[Player.OPPONENT]
-        self._duel_cache = copy.deepcopy(self._duel_cache)
-        return reward
-
-
-    def _print_match_result(self, win_on_match: bool) -> None:
-        result = ' win' if win_on_match else 'lose'
-        win_rate: float = self._wins/(((self._match_count-1)%100)+1)
-        print(f'Match {self._match_count:4d}: {result}, {win_rate:%}')
-        if self._match_count % 100 == 0:
-            self._wins = 0
-
-    
-    def _dump_match_result(self) -> None:
-        if len(self._match_result) == 0:
-            return
-        now = datetime.datetime.now().isoformat(timespec='seconds').replace(':', '-')
-        file = Path.cwd() / 'Decks' / self._deck.name / (now + '.csv')
-        with file.open(mode='w', newline='') as f:
-            writer = csv.writer(f)
-            writer.writerow(('match', 'win'))
-            writer.writerows(enumerate(self._match_result))
-        self._match_result.clear()
-  
-    
-    def _update_usedflag(self, card_id: int) -> None:
-        self._usedflag.used(card_id)
-
-
-    def select_tp(self) -> bool:
-        return True
-        
-
-    def select_mainphase_action(self, main: MainPhase) -> int:
-        choices: list[Choice] = []
-
-        for index, card in enumerate(main.summonable):
-            choices.append(Choice(Action.SUMMON, index, card.id))
-        
-        for index, card in enumerate(main.special_summonable):
-            choices.append(Choice(Action.SP_SUMMON, index, card.id))
-
-        for index, card in enumerate(main.repositionable):
-            choices.append(Choice(Action.REPOSITION, index, card.id))
-
-        for index, card in enumerate(main.moster_settable):
-            choices.append(Choice(Action.SET_MONSTER, index, card.id))
-
-        for index, card in enumerate(main.spell_settable):
-            choices.append(Choice(Action.SET_SPELL, index, card.id))
-
-        for index, card in enumerate(main.activatable):
-            desc: int = main.activation_descs[index]
-            choices.append(Choice(Action.ACTIVATE, index, card.id, desc))
-
-        if main.can_battle:
-            choices.append(Choice(Action.BATTLE))
-
-        if main.can_end:
-            choices.append(Choice(Action.END))
-
-        selected: Choice = self._brain.select(choices, self._duel, self._usedflag)    
-        if selected.action is Action.ACTIVATE:
-            self._update_usedflag(selected.card_id)
-
-        return (selected.index << 16) + Action_to_int(selected.action)
-
-
-    def select_battle_action(self, battle: BattlePhase) -> int:
-        choices: list[Choice] = []
-
-        for index, card in enumerate(battle.attackable):
-            choices.append(Choice(Action.ATTACK, index, card.id))
-
-        for index, card in enumerate(battle.activatable):
-            desc: int = battle.activation_descs[index]
-            choices.append(Choice(Action.ACTIVATE_IN_BATTLE, index, card.id, option=desc))
-
-        if battle.can_main2:
-            choices.append(Choice(Action.MAIN2))
-
-        selected: Choice = self._brain.select(choices, self._duel, self._usedflag)
-        if selected.action == Action.ACTIVATE_IN_BATTLE:
-            self._update_usedflag(selected.card_id)
-
-        return (selected.index << 16) + Action_to_int(selected.action)
-
-
-    def select_effect_yn(self, card: Card, desc: int) -> bool:
-        return True
-
-
-    def select_yn(self) -> bool:
-        return True
-
-
-    def select_battle_replay(self) -> bool:
-        return True
-
-    
-    def select_option(self, options: list[int]) -> int:
-        return random.choice(list(range(len(options))))
-
-
-    def select_card(self, cards: list[Card], min_: int, max_: int, cancelable: bool, hint: int) -> list[int]:
-        choices: list[Choice] = []
-        for index, card in enumerate(cards):
-            choices.append(Choice(Action.SELECT, index, card.id, option=hint))
-
-        num_to_select: int = max_ # ToDo: more intelligent
-        selecteds: list[Choice] = []
-        for _ in range(max_):
-            selected: Choice = self._brain.select(choices, self._duel, self._usedflag)
-            selecteds.append(selected)
-            choices.remove(selected)
-        return [selected.index for selected in selecteds]
-
-
-    def select_chain(self, cards: list[Card], descriptions: list[int], forced: bool) -> int:
-        choices: list[Choice] = []
-
-        for index, card in enumerate(cards):
-            desc = descriptions[index]
-            choices.append(Choice(Action.CHAIN, index, card.id, desc))
-
-        if not forced:
-            choices.append(Choice(Action.CHAIN, -1, card_id=0, option=0)) # -1 means no activation
-
-        selected: Choice = self._brain.select(choices, self._duel, self._usedflag)
-        if selected.index != -1:
-            self._update_usedflag(selected.card_id)
-
-        return selected.index
-
-
-    def select_place(self, player: Player, location: Location, selectable: int, is_pzone: bool) -> int:
-        zones: list[Zone] = self._duel.field[player].where(location)
-        choices: list[int] = [i for i, zone in enumerate(zones) if bool(selectable & zone.id)]
-        ans: int = random.choice(choices)
-        
-        if is_pzone:
-            ans = 6 if ans == 0 else 7
-
-        return ans 
-
-
-    def select_position(self, card_id: int, choices: list[CardPosition]) -> int:
-        return int(choices[0])
-
-
-    def select_tribute(self, choices: list[Card], min_: int, max_: int, cancelable: bool, hint: int) -> list[int]:
-        my_card: list[Card] = sorted([card for card in choices if card.controller == Player.ME], key=lambda x:x.attack)
-        op_card: list[Card] = sorted([card for card in choices if card.controller == Player.OPPONENT], key=lambda x:-x.attack)
-        choosed: list[Card] = (op_card + my_card)[0:max_]
-        return [choices.index(card) for card in choosed]
+    def _create_eval_actor(self) -> actor.Actor:
+        return actor.Actor(
+            self._eval_env,
+            self._eval_policy,
+            self._train_step,
+            episodes_per_run=_num_eval_episodes,
+            metrics=actor.eval_metrics(_num_eval_episodes),
+            summary_dir=os.path.join(tempdir, 'eval')
+        )
     
 
-    def select_sum(self, choices: list[tuple[Card, int, int]], sum_value: int, min_: int, max_: int, must_just: bool, select_hint: int) -> list[int]:
-        raise Exception('not complete coding')
+    def _create_agent_learner(self) -> learner.Learner:
+        learning_triggers = [
+            triggers.PolicySavedModelTrigger(
+                os.path.join(tempdir, learner.POLICY_SAVED_MODEL_DIR),
+                self._agent,
+                self._train_step,
+                interval=_policy_save_interval
+            ),
+            triggers.StepPerSecondLogTrigger(self._train_step, interval=1000)
+        ]
+
+        return learner.Learner(
+            tempdir,
+            self._train_step,
+            self._agent,
+            lambda: self._reverb_replay.as_dataset(sample_batch_size=_batch_size, num_steps=2).prefetch(50),
+            triggers=learning_triggers
+        )
 
 
-    def select_unselect(self, choices: list[Card], min_: int, max_: int, cancelable: bool, hint: int):
-        return self.select_card(choices, min_, max_, cancelable, hint)
-
-
-    def select_counter(self, counter_type: int, quantity: int, cards: list[Card], counters: list[int]) -> list[int]:
-        raise Exception('not complete coding')
-
-
-    def select_number(self, choices: list[int]) -> int:
-        raise Exception('not complete coding')
+    def _get_eval_metrics(self) -> dict:
+        self._eval_actor.run()
+        results = {}
+        for metric in self._eval_actor.metrics:
+            results[metric.name] = metric.result()
+        return results
 
     
-    def sort_card(self, cards: list[Card]) -> list[int]:
-        raise Exception('not complete coding')
+    def _log_eval_metrics(self, step: int, metrics: dict) -> None:
+        eval_results = ', '.join(f'{name} = {result:.6f}' for name, result in metrics.items())
+        print(f'step = {step}: {eval_results}')
 
+    
+    def train(self, iterations: int) -> None:
+        self._agent.train_step_counter.assign(0)
 
-    def announce_attr(self, choices: list[Attribute], count: int) -> list[int]:
-        raise Exception('not complete coding')
+        returns = [self._get_eval_metrics()['AverageReturn']]
 
+        for _ in range(iterations):
+            self._collect_actor.run()
+            loss_info = self._agent_learner.run(num_iterations=1)
 
-    def announce_race(self, choices: list[Race], count: int) -> list[int]:
-        raise Exception('not complete coding')
+            step = int(self._agent_learner.train_step_numpy)
 
+            if step % _eval_interval == 0:
+                metrics = self._get_eval_metrics()
+                self._log_eval_metrics(step, metrics)
+                returns.append(metrics['AverageReturn'])
 
-        
+            if step % _log_interval == 0:
+                print(f'step = {step}: loss = {loss_info.loss.numpy()}')
 
-def Action_to_int(action: Action) -> int:
-    convert = {
-        Action.SUMMON:     0,
-        Action.SP_SUMMON:  1,
-        Action.REPOSITION: 2,
-        Action.SET_MONSTER:3,
-        Action.SET_SPELL:  4,
-        Action.ACTIVATE:   5,
-        Action.BATTLE:     6,
-        Action.END:        7,
-
-        Action.ACTIVATE_IN_BATTLE: 0,
-        Action.ATTACK:             1,
-        Action.MAIN2:              2,
-        Action.END_IN_BATTLE:      3,
-
-        Action.CHAIN:  20,
-        Action.SELECT: 21
-    }
-    return convert[action]
+        #self._rb_observer.close()
+        #self._reverb_server.stop()
