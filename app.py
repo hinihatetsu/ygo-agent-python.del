6dfda752de26c42975dee77c047935c9da45c413
hinihatetsu <shuheinittalr@gmail.com>
2021-04-21 17:34:56 +0000

    app sample

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
 from util import LaunchInfo, load_args
-from pyYGOenvironment import GameClient
-from pyYGOagent import DuelAgent
+from pyYGOenv import YGOEnv
+from pyYGOagent.agent import DuelAgent
 
 
 def main():
     info: LaunchInfo = load_args()
-    agent = DuelAgent(info.deck, info.notrain)
-    client = GameClient(info.host, info.port, info.version, info.name)
-    agent.set_client(client)
-    agent.run()
+    collect_env = YGOEnv(info.deck, info.host, info.port, info.version, info.name+'_collect')
+    eval_env = YGOEnv(info.deck, info.host, info.port+1, info.version, info.name+'_eval')
+    agent = DuelAgent(collect_env, eval_env)
+    agent.train(10000)
+    collect_env.close()
+    eval_env.close()
 
 
 if __name__ == '__main__':
